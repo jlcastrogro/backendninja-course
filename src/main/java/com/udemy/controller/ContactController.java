3ca364248acf83559bdbba01d9113197e0baeeda
José Luis <jlcastrogro@gmail.com>
2018-09-30 14:05:40 -0500

    Agregado formulario para agregar contactos

--- src/main/java/com/udemy/controller/ContactController.java
+++ src/main/java/com/udemy/controller/ContactController.java
@@ -1,16 +1,42 @@
 package com.udemy.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.udemy.constants.ViewConstant;
+import com.udemy.model.ContactModel;
 
 @Controller
 @RequestMapping("/contact")
 public class ContactController {
+	private static final Log LOGGER = LogFactory.getLog(ContactController.class);
+
+	@GetMapping("")
+	public String showContact() {
+		return ViewConstant.CONTACTS;
+	}
+
+	@GetMapping("/cancel")
+	public String cancel() {
+		return ViewConstant.CONTACTS;
+	}
+
 	@GetMapping("/form")
-	public String redirectForm() {
+	public String redirectToContactForm(Model model) {
+		model.addAttribute("contact", new ContactModel());
 		return ViewConstant.CONTACT_FORM;
+	}
+
+	@PostMapping("")
+	public String addContact(@ModelAttribute(name = "contact") ContactModel contactModel, Model model) {
+		LOGGER.info("Method: 'addContact()' -- Params: '" + contactModel + "'");
+		model.addAttribute("result", 1);
+		return ViewConstant.CONTACTS;
 	}
 }
