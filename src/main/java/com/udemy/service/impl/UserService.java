b877683b8c5621f25bf7ba0a1062ab21e5c35963
José Luis <jlcastrogro@gmail.com>
2018-10-01 01:27:11 -0500

    Configuraración de Spring Security

--- src/main/java/com/udemy/service/impl/UserService.java
+++ src/main/java/com/udemy/service/impl/UserService.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.authority.mapping.Attributes2GrantedAuthoritiesMapper;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
