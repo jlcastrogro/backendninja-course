97a1dcf437cca34815a5285a6c1d277a3d65e7fb
José Luis <jlcastrogro@gmail.com>
2018-10-01 16:56:37 -0500

    Procesamiento por lotes con Sptring Batch

---
+++ src/main/java/com/udemy/component/TaskComponent.java
@@ -0,0 +1,18 @@
+package com.udemy.component;
+
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component("taskComponent")
+public class TaskComponent {
+	private static final Log LOGGER = LogFactory.getLog(TaskComponent.class);
+	
+	@Scheduled(fixedDelay = 5000)
+	public void doTask() {
+		LOGGER.info("TIME IS: " + new Date());
+	}
+}
