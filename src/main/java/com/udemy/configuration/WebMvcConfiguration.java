90042b7dc3cc4ce61a076d56741e6df79b59a33a
José Luis <jlcastrogro@gmail.com>
2018-10-02 08:05:48 -0500

    Modificado clase WebMvcConfigurerAdapter por la interfaz WebMvcConfigurer

--- src/main/java/com/udemy/configuration/WebMvcConfiguration.java
+++ src/main/java/com/udemy/configuration/WebMvcConfiguration.java
@@ -4,21 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.udemy.component.RequestTimeInterceptor;
 
 @Configuration
-public class WebMvcConfiguration extends WebMvcConfigurerAdapter{
-	
+public class WebMvcConfiguration implements WebMvcConfigurer{
 	@Autowired
 	@Qualifier("requestTimeInterceptor")
 	private RequestTimeInterceptor requestTimeInterceptor;
 	
-	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(requestTimeInterceptor);
 	}
-
-	
 }
